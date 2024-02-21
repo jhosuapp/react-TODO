b45619b4c3b0a279b6d87eacbb143c0c070c0d2f
jhosuapp <jhosuapvll@gmail.com>
2024-02-21 16:35:59 -0500

    refactor: app.js

--- src/js/App.js
+++ src/js/App.js
@@ -4,17 +4,12 @@ import {useState, useEffect} from 'react';
 import '../scss/main.scss';
 import '../scss/templates/_todo.scss';
 //Components
-import { TodoItem } from './components/TodoItem';
-import { TodoCounter } from './components/TodoCounter';
-import { TodoList } from './components/TodoList';
-import { TodoCreateTask } from './components/TodoCreateTask';
-import { TodoCtn } from './components/TodoCtn';
-import { TodoSearch } from './components/TodoSearch';
-import { TodoDefault } from './components/TodoDefault';
 import { TodoAccents } from './components/TodoAccents';
-import { TodoResults } from './components/TodoResults'; 
+import { TodoDefault } from './components/TodoDefault';
 //Custom hooks
 import { useLocalStorage } from './hooks/useLocalStorage';
+//App ui
+import { AppUi } from './AppUi';
 
 const App = ()=>{
   //States
@@ -47,32 +42,17 @@ const App = ()=>{
   }
 
   return (
-    <>
-      <TodoCtn>
-        {/* Create a task */}
-        <section>
-          <TodoCreateTask />
-        </section>
-        {/* List task, search task and feedback task */}
-        <section>
-          <TodoCounter completed={completedTodo} total={totalTodo} />
-          <TodoSearch stateValue={stateValue} setStateValue={setStateValue}/>
-          <TodoResults results={filterTodo.length} cls={!showResults && 'hide'}/>
-          <TodoList>
-            {filterTodo.map((data)=>(
-              <TodoItem 
-                key={data.text}
-                text={data.text}
-                completed={data.completed}
-                onComplete={()=>{completeTodo(data.text)}}
-                onDelete={()=>{deleteTodo(data.text)}}
-              />
-            ))}
-          </TodoList>
-        </section>
-      </TodoCtn>
-    </>
-  );
+    <AppUi 
+      completedTodo={completedTodo}
+      totalTodo={totalTodo}
+      stateValue={stateValue}
+      setStateValue={setStateValue}
+      filterTodo={filterTodo}
+      showResults={showResults}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
+  )
 }
 
 export default App;
---
+++ src/js/AppUi.js
@@ -0,0 +1,49 @@
+//Components
+import { TodoItem } from './components/TodoItem';
+import { TodoCounter } from './components/TodoCounter';
+import { TodoList } from './components/TodoList';
+import { TodoCreateTask } from './components/TodoCreateTask';
+import { TodoCtn } from './components/TodoCtn';
+import { TodoSearch } from './components/TodoSearch';
+import { TodoResults } from './components/TodoResults'; 
+
+const AppUi = ({
+    completedTodo,
+    totalTodo,
+    stateValue,
+    setStateValue,
+    filterTodo,
+    showResults,
+    completeTodo,
+    deleteTodo
+})=>{
+    return (
+        <>
+          <TodoCtn>
+            {/* Create a task */}
+            <section>
+              <TodoCreateTask />
+            </section>
+            {/* List task, search task and feedback task */}
+            <section>
+              <TodoCounter completed={completedTodo} total={totalTodo} />
+              <TodoSearch stateValue={stateValue} setStateValue={setStateValue}/>
+              <TodoResults results={filterTodo.length} cls={!showResults && 'hide'}/>
+              <TodoList>
+                {filterTodo.map((data)=>(
+                  <TodoItem 
+                    key={data.text}
+                    text={data.text}
+                    completed={data.completed}
+                    onComplete={()=>{completeTodo(data.text)}}
+                    onDelete={()=>{deleteTodo(data.text)}}
+                  />
+                ))}
+              </TodoList>
+            </section>
+          </TodoCtn>
+        </>
+      );
+}
+
+export { AppUi }
